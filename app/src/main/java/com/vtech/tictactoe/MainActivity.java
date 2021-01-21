e1baefb171985c06da360e915848c7d3135c7dd8
vinod <vinodsilva.10@gmail.com>
2021-01-22 01:28:01 +0530

    Basic Ui with functions

--- app/src/main/java/com/vtech/tictactoe/MainActivity.java
+++ app/src/main/java/com/vtech/tictactoe/MainActivity.java
@@ -4,11 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+     //yellow = 0 ,red =1]
+     int  activePlayer = 0;
 
      public void dropIn(View view){
+         ImageView counter = (ImageView) view ;
+         counter.setTranslationY(-1000f);
+
+         if (activePlayer==0){
+             counter.setImageResource(R.drawable.yellow);
+             activePlayer =1;
+         }else{
+             counter.setImageResource(R.drawable.red);
+             activePlayer =0;
+         }
+         
+         counter.animate().translationYBy(1000f).rotation(360).setDuration(300);
 
      }
 
