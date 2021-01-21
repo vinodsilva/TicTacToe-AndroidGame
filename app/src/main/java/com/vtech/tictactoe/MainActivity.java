f061db6c918bb196976c19280e463d4efb2cd52e
vinod <vinodsilva.10@gmail.com>
2021-01-22 00:59:24 +0530

    initialize

---
+++ app/src/main/java/com/vtech/tictactoe/MainActivity.java
@@ -0,0 +1,20 @@
+package com.vtech.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+     public void dropIn(View view){
+
+     }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
